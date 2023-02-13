9770b6e0bdf89e3edcda5add58b4a4129c3d5ffd
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-02-14 00:31:42 +0800

    function to get correct

--- SpellChecker.py
+++ SpellChecker.py
@@ -5,4 +5,10 @@ def clearAll():
     word1_field.delete(0, END)
     word2_field.delete(0, END)
 
+def correction():
+    input_word = word1_field.get()
+    word = TextBlob(input_word)
+    corrected_word = str(word.correct())
+    word2_field.insert(10, corrected_word)
+
 
