9c8852a25bc8cc773f346f17e7a19b99f7763657
Angelica Delmendo <anggedelmendo08@gmail.com>
2023-02-14 21:04:59 +0800

    driving the code

--- SpellChecker.py
+++ SpellChecker.py
@@ -8,5 +8,10 @@ from textblob import TextBlob
 def clearAll():
     word1_field.delete(0, END)
     word2_field.delete(0, END)
+def correction():
+    input_word = word1_field.get()
+    word = TextBlob(input_word)
+    corrected_word = str(word.correct())
+    word2_field.insert(10, corrected_word)
 
 
